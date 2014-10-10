e259c855d355d3378e1b4457347899cc63d1c8c1
Michael Fogleman <michaelwfogleman@gmail.com>
2014-10-09 21:13:36 -0400

    First stabs at dispatching arguments for the jar.
    lein uberjar to compile, cd to target/uberjar, and then run a
    command, like this:
    
    java -jar keepintouch-0.1.0-SNAPSHOT-standalone.jar
    /home/michael/Dropbox/keepintouch.data weight
    
    Currently, providing a weight like 0.13 won't work from the jar...

--- clj/keepintouch/src/keepintouch/core.clj
+++ clj/keepintouch/src/keepintouch/core.clj
@@ -5,5 +5,16 @@
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args])
+  "Takes a file and optional schedules."
+  ([] (println "I was expecting a file path!"))
+  ([file] (backlog file))
+  ([file schedule] (backlog file))
+  ([file schedule & more]
+     (let [f (first more)
+           s (second more)]
+       (cond (= f "backlog") (backlog file)
+             (= f "random") (randlog file)
+             (and (= f "weight")
+                  (number? s)) ;; somehow this part is not working
+             (weightlog file s)
+             (= f "weight") (weightlog file 0.25)))))
