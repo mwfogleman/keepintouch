d04f010dd7a23c873eda503fe61e00b9aa9540b2
Michael Fogleman <michaelwfogleman@gmail.com>
2014-10-15 09:25:53 -0400

    Add tests for kit-update.

--- clj/keepintouch/test/keepintouch/core_test.clj
+++ clj/keepintouch/test/keepintouch/core_test.clj
@@ -20,3 +20,16 @@
              (keys (first (shuffle (kit-in test-file)))) => '(:interval :contacted :names))
        (fact "each map's values are still strings"
              (vals (first (shuffle (kit-in test-file)))) => (partial map string?)))
+
+(facts "about kit-update"
+       (fact "it takes the product of kit-in, and returns a new list"
+             (map kit-update (kit-in test-file)) => list?)
+       (fact "that list still contains maps"
+             (map kit-update (kit-in test-file)) => (partial map map?))
+       ;; The aforementioned (ab)use of shuffle is present here, too
+       (fact "and each map's keys are still :interval, :contacted, and :names"
+             (keys (first (shuffle (map kit-update (kit-in test-file))))) => '(:interval :contacted :names))
+       (fact "but the :interval values are now numbers"
+             (:interval (first (shuffle (map kit-update (kit-in test-file))))) => number?)
+       (fact "and the :contacted values are now date objects"
+             (:contacted (first (shuffle (map kit-update (kit-in test-file))))) => (partial instance? org.joda.time.DateTime)))
