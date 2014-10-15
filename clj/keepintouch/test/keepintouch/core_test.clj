4617c8ec181d8277cc0b439458e0550f24857755
Michael Fogleman <michaelwfogleman@gmail.com>
2014-10-15 08:58:53 -0400

    Add tests for kit-in.

--- clj/keepintouch/test/keepintouch/core_test.clj
+++ clj/keepintouch/test/keepintouch/core_test.clj
@@ -7,16 +7,16 @@
 
 (def today (t/date-time 2011 12 11))
 
-(def test-file
-"30
-2011/12/01
-Recently Contacted
+(def test-file "test/keepintouch/test-file.data")
 
-180
-2011/11/01
-Every Six-Months
-
-60
-2008/7/14
-Really Overdue")
-
+(facts "about kit-in"
+       (fact "it takes a keepintouch.data file, and produces a list"
+             (kit-in test-file) => list?)
+       (fact "that list contains maps"
+             (kit-in test-file) => (partial map map?))
+       ;; I used shuffle in this next example to prove that the fact is true for any of the maps
+       ;; There is probably a more idiomatic way of doing this
+       (fact "each map's keys are :interval, :contacted, and :names"
+             (keys (first (shuffle (kit-in test-file)))) => '(:interval :contacted :names))
+       (fact "each map's values are still strings"
+             (vals (first (shuffle (kit-in test-file)))) => (partial map string?)))
