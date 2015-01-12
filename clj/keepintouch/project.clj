e132fb1864cf38edbf82c1d75c28e70c357bb4d1
Michael Fogleman <michaelwfogleman@gmail.com>
2015-01-11 22:58:38 -0500

    Upgrade Clojure, Midje, and clj-time.
    Tests are still passing.

--- clj/keepintouch/project.clj
+++ clj/keepintouch/project.clj
@@ -1,11 +1,11 @@
 (defproject keepintouch "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [org.clojure/tools.reader "0.8.13"]
-                 [clj-time "0.8.0"]]
+                 [clj-time "0.9.0"]]
   :main ^:skip-aot keepintouch.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev
-             {:dependencies [[midje "1.6.3"]]}})
+             {:dependencies [[midje "1.7.0-SNAPSHOT"]]}})
