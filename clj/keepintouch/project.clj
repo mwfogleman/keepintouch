f80ff42a6a52f839767fd3e31c0358fcc6980e86
Michael Fogleman <michaelwfogleman@gmail.com>
2015-01-12 08:47:59 -0500

    Add squiggly-clojure configuration.

--- clj/keepintouch/project.clj
+++ clj/keepintouch/project.clj
@@ -3,9 +3,13 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [org.clojure/tools.reader "0.8.13"]
-                 [clj-time "0.9.0"]]
+                 [clj-time "0.9.0"]
+                 [acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]]
   :main ^:skip-aot keepintouch.core
   :target-path "target/%s"
+  :plugins [[lein-environ "1.0.0"]]
   :profiles {:uberjar {:aot :all}
              :dev
-             {:dependencies [[midje "1.7.0-SNAPSHOT"]]}})
+             {:dependencies [[midje "1.7.0-SNAPSHOT"]]
+              :env {:squiggly {:checkers [:eastwood :kibit]
+                               :eastwood-exclude-linters [:unlimited-use]}}}})
