6e0d112021db90e077b6e5e27faa9a84bad15472
Michael Fogleman <michaelwfogleman@gmail.com>
2016-12-25 05:46:32 -0500

    Update lein ancient results, and Clojure.

--- clj/keepintouch/project.clj
+++ clj/keepintouch/project.clj
@@ -1,15 +1,15 @@
 (defproject keepintouch "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                 [org.clojure/tools.reader "0.8.13"]
-                 [clj-time "0.9.0"]
-                 [acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.reader "0.10.0"]
+                 [clj-time "0.12.2"]
+                 [acyclic/squiggly-clojure "0.1.6"]]
   :main ^:skip-aot keepintouch.core
   :target-path "target/%s"
   :plugins [[lein-environ "1.0.0"]]
   :profiles {:uberjar {:aot :all}
              :dev
-             {:dependencies [[midje "1.7.0-SNAPSHOT"]]
+             {:dependencies [[midje "1.8.3"]]
               :env {:squiggly {:checkers [:eastwood :kibit]
                                :eastwood-exclude-linters [:unlimited-use]}}}})
