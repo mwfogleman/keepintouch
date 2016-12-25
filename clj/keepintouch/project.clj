677d3cadcdbde0c925fcabf4d5d6384d8eee51e2
Michael Fogleman <michaelwfogleman@gmail.com>
2016-12-25 05:47:31 -0500

    Add descriptions and URL, bump snap version.

--- clj/keepintouch/project.clj
+++ clj/keepintouch/project.clj
@@ -1,6 +1,6 @@
-(defproject keepintouch "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject keepintouch "0.2.0-SNAPSHOT"
+  :description "Keep in Touch with friends and family"
+  :url "https://www.github.com/mwfogleman/keepintouch"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.reader "0.10.0"]
                  [clj-time "0.12.2"]
